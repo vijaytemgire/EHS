179a1bc5c4e3ece5fbba3630f5b3f25f163fb054
Vijay Temgire <vijay.temgire@accenture.com>
2023-05-23 08:10:33 +0000

    Initial Commit @5/23/2023, 8:10:33 AM

---
+++ webapp/changes/coding/zdetail.js
@@ -0,0 +1,99 @@
+/***
+@controller Name:ehs.inc.reportincidents1.controller.CreateIncident,
+*@viewId:application-adaptationproject-display-component---createIncident
+*/
+/*!
+ * OpenUI5
+ * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
+ * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
+ */
+
+sap.ui.define([
+		'sap/ui/core/mvc/ControllerExtension'
+		,'sap/ui/core/mvc/OverrideExecution'
+	],
+	function (
+		ControllerExtension
+		,OverrideExecution
+	) {
+		"use strict";
+		return ControllerExtension.extend("customer.EHS.zdetail", {
+			metadata: {
+				// extension can declare the public methods
+				// in general methods that start with "_" are private
+				methods: {
+					publicMethod: {
+						public: true /*default*/ ,
+						final: false /*default*/ ,
+						overrideExecution: OverrideExecution.Instead /*default*/
+					},
+					finalPublicMethod: {
+						final: true
+					},
+					onMyHook: {
+						public: true /*default*/ ,
+						final: false /*default*/ ,
+						overrideExecution: OverrideExecution.After
+					},
+					couldBePrivate: {
+						public: false
+					}
+				}
+			},
+
+			// adding a private method, only accessible from this controller extension
+			_privateMethod: function() {},
+			// adding a public method, might be called from or overridden by other controller extensions as well
+			publicMethod: function() {},
+			// adding final public method, might be called from, but not overridden by other controller extensions as well
+			finalPublicMethod: function() {},
+			// adding a hook method, might be called by or overridden from other controller extensions
+			// override these method does not replace the implementation, but executes after the original method
+			onMyHook: function() {},
+			// method public per default, but made private via metadata
+			couldBePrivate: function() {},
+			// this section allows to extend lifecycle hooks or override public methods of the base controller
+			override: {
+				/**
+				 * Called when a controller is instantiated and its View controls (if available) are already created.
+				 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+				 * @memberOf customer.EHS.zdetail
+				 */
+				onInit: function() {
+				},
+
+				/**
+				 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+				 * (NOT before the first rendering! onInit() is used for that one!).
+				 * @memberOf customer.EHS.zdetail
+				 */
+				onBeforeRendering: function() {
+				},
+
+				/**
+				 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+				 * This hook is the same one that SAPUI5 controls get after being rendered.
+				 * @memberOf customer.EHS.zdetail
+				 */
+				onAfterRendering: function() {
+					
+					this.getView().byId("assetRadioButtonLabel").setVisible(false);
+					this.getView().byId("assetRadioButton").setVisible(false);
+					this.getView().byId("assetRadioButtonYes").setVisible(false);
+					this.getView().byId("assetRadioButtonNo").setVisible(false);
+					
+				},
+
+				/**
+				 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+				 * @memberOf customer.EHS.zdetail
+				 */
+				onExit: function() {
+				},
+
+				// override public method of the base controller
+				// basePublicMethod: function() {
+				// }
+			}
+		});
+	});
